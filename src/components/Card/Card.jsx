9ce383e8cf24aeb0b2a0951430943feb95c7ec6c
nilton <ntuestabazan@gmail.com>
2023-05-13 13:00:10 +0200

    card title

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,10 @@ import "./Card.css";
 export function Card() {
   return (
     <article className="box-card">
-      <div className="card-title"></div>
+      <div className="card-title">
+        <h3>Bulbasaur</h3>
+        <h5>#001</h5>
+      </div>
       <div className="card-image"></div>
       <div className="card-description"></div>
     </article>
